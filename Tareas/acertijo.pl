d437709a089fa9f29a6c3ccb361edfc6c6c9e428
Gilberto López <gilisaac@ciencias.unam.mx>
2017-03-16 19:47:51 -0600

    Correspondencia 1 a 1 entre los helados y las personas

--- Tareas/acertijo.pl
+++ Tareas/acertijo.pl
@@ -79,6 +79,11 @@ costo(Helado,Precio) :- member(Helado,[cono,copa,tarrina]),
 
 %% Isidrio pidió bombón.
 pidio(isidro,bombon).
+%% El novio y Felipe no pidieron tarrina.
+pidio(Nombre,tarrina) :- nombre(Nombre),
+                         parentezco(Novio,novio),
+                         Nombre \== Novio,
+                         Nombre \== felipe.
 %% Nuria pidio el más caro.
 pidio(nuria,Helado) :- helado(Helado),
                        costo(Helado,N),
@@ -88,13 +93,7 @@ pidio(Madre,Helado) :- helado(Helado),
                        parentezco(Madre,madre),
                        costo(Helado,N),
                        forall(precio(M), N @=< M).
-%% El novio no pidio tarrina.
-pidio(Nombre,tarrina) :- nombre(Nombre),
-                         parentezco(Novio,novio),
-                         Nombre \== Novio.
-%% Felipe no pidió tarrina.
-pidio(Nombre,tarrina) :- nombre(Nombre),
-                         Nombre \== felipe.
+
 %% Posibles opciones de helados.
 pidio(Nombre,Helado) :- nombre(Nombre),
                         helado(Helado).
@@ -108,8 +107,16 @@ solucion(Nombre1,Nombre2,Nombre3,Nombre4,Nombre5,
 nombre(Nombre1),nombre(Nombre2),nombre(Nombre3),nombre(Nombre4),nombre(Nombre5),
 parentezco(Nombre1,Relacion1),parentezco(Nombre2,Relacion2),parentezco(Nombre3,Relacion3),parentezco(Nombre4,Relacion4),parentezco(Nombre5,Relacion5),
 pidio(Nombre1,Helado1),pidio(Nombre2,Helado2),pidio(Nombre3,Helado3),pidio(Nombre4,Helado4),pidio(Nombre5,Helado5),
-costo(Helado1,Costo1),costo(Helado2,Costo2),costo(Helado3,Costo3),costo(Helado4,Costo4),costo(Helado5,Costo5).
+costo(Helado1,Costo1),costo(Helado2,Costo2),costo(Helado3,Costo3),costo(Helado4,Costo4),costo(Helado5,Costo5),
+Costo1 =:= Costo2,Costo1 =:= Costo3,Costo1 =:= Costo4,Costo1 =:= Costo5,
+Costo2 =:= Costo3,Costo2 =:= Costo4,Costo2 =:= Costo5,
+Costo3 =:= Costo4,Costo3 =:= Costo4,
+Costo4 =:= Costo5,
+Helado1 \== Helado2,Helado1 \== Helado3,Helado1 \== Helado4,Helado1 \== Helado5,
+Helado2 \== Helado3,Helado2 \== Helado4,Helado2 \== Helado5,
+Helado3 \== Helado4,Helado3 \== Helado4,
+Helado4 \== Helado5.
 
 %% Prueba
-%% solucion(elisa,felipe,isidro,luis,nuria,R1,R2,R3,R4,R5,H1,H2,H3,H4,H5,C1,C2,C3,C4,C5).
+%% solucion(elisa,felipe,isidro,luis,nuria,madre,padre,hermano,novio,hermana,H1,H2,H3,H4,H5,C1,C2,C3,C4,C5).
 
