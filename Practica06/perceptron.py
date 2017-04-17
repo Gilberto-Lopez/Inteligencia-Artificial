95f0411fbc2ec00530832435b794796a35fb1e7d
Gilberto-López <gilisaac@ciencias.unam.mx>
2017-04-17 13:41:19 -0500

    Método auxiliar _entrenamiento para el proceso de entrenamiento.

--- Practica06/perceptron.py
+++ Practica06/perceptron.py
@@ -1,39 +1,53 @@
 from random import uniform
+import math
 
 class Perceptron (object):
 	"""
 	Clase Perceptron para modelar un perceptón.
 	"""
 
-	def __init__ (self, pesos = 4, activacion):
+	def __init__ (self, pesos = 4, activacion, tasa_aprendizaje = 0.1):
 		"""
 		Inicializa un perceptrón con una cantidad fija de pesos
 		para las entradas (por defecto 4), establecidos como
 		números aleatorios en el intervalo [-0.5,0.5], y un
 		umbral para comparar la salida del perceptrón en ese
 		mismo intervalo. También recibe la función de activación
-		para el preceptrón
+		para el preceptrón y la tasa de aprendizaje, en el intervalo (0,1)
 		:param pesos: La cantidad de pesos/entradas del perceptrón.
 		:param activacion: La función de activación.
+		:param tasa_aprendizaje: La tasa de aprendizaje.
 		"""
 		self.entradas = pesos
 		self.pesos = [uniform (-0.5,0.5) for _ in range (pesos)]
-		self.umbral = uniform (-0.5,0.5)
-		self.activacion = activacion
+		self.theta = uniform (-0.5,0.5)
+		self.f = activacion
+		self.alpha = tasa_aprendizaje if 0 < tasa_aprendizaje < 1 else 0.1
 
 	def salida (self, entradas):
 		"""
 		Calcula la salida del perceptrón para un ejemplar dado.
 		La salida del perceptrón está dada por:
-			Y = f(Sum_i (w_i.x_i) - umbral)
-		donde x_i es el i-ésimo valor de entrada, w_i el i-ésimo peso para x_i
-		y f es la función de activación del perceptrón.
+			Y = f(Sum i (w_i.x_i) - theta)
+		donde x_i es el i-ésimo valor de entrada, w_i el i-ésimo peso para x_i,
+		theta el umbral y f es la función de activación del perceptrón.
 		:param entradas: Los valores de entrada para el perceptrón (ejemplar).
 		"""
 		if len (entradas) != self.entradas:
-			return 0
-		suma_ponderada = sum (w_i * x_i for (w_i, x_i) in zip (self.pesos, entradas))
-		return self.activacion (suma_ponderada - self.umbral)
+			return float('nan')
+		suma_ponderada = sum ([w_i * x_i for (w_i, x_i) in zip (self.pesos, entradas)])
+		return self.f (suma_ponderada - self.theta)
 
-	def entrenamiento (self):
+	def _entrenamiento (self, ejemplar, salida):
+		# Método auxiliar para entrenamiento(), calcula el error del perceptrón
+		# con un ejemplar dado y actualiza los pesos del perceptrón.
+		salida_perceptron = self.salida (ejemplar)
+		if math.isnan (salida_perceptron):
+			return salida_perceptron
+		error = salida - salida_perceptron
+		if error != 0:
+			self.pesos = [w_i + self.alpha * x_i * error for (w_i, x_i) in zip (self.pesos, ejemplar)]
+		return error
+
+	def entrenamiento (self, conjunto, salida):
 		pass
