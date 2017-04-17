2577b6148590222ff458e20371ec90bf1c7522a7
Gilberto-López <gilisaac@ciencias.unam.mx>
2017-04-17 14:35:55 -0500

    init para el intérprete de Python.

---
+++ Practica06/__init__.py
@@ -0,0 +1,6 @@
+# La clase Perceptron está disponible para todo el paquete Practica06.
+from perceptron import Perceptron
+
+# Módulos que se importan con:
+#     from Practica06 import *
+__all__ = ['perceptron']
