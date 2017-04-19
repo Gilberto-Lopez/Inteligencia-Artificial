b022a10300901cca20168edf5f571a7802568948
Gilberto López <gilisaac@ciencias.unam.mx>
2017-04-18 23:29:37 -0500

    Clases PerceptronAND para la operación AND y PerceptronOR para la operación OR.

---
+++ Practica06/perceptron_and.py
@@ -0,0 +1,17 @@
+from Practica06 import Perceptron
+from itertools import product
+
+class PerceptronAND(Perceptron):
+	"""
+	Clase PerceptronAND para modelar un perceptrón que reconozca la operación
+	lógica AND de tres variables.
+	"""
+
+	def __init__(self):
+		"""
+		Inicializa el perceptrón usando como función de activación la función
+		escalón.
+		"""
+		super (PerceptronAND, self).__init__ (3,lambda x: 0 if x < 0 else 1)
+		self.tabla_and = map(list, product (*([[0,1]]*3)))
+		self.salida = [0 for _ in range(7)] + [1]
---
+++ Practica06/perceptron_or.py
@@ -0,0 +1,17 @@
+from Practica06 import Perceptron
+from itertools import product
+
+class PerceptronOR(Perceptron):
+	"""
+	Clase PerceptronOR para modelar un perceptrón que reconozca la operación
+	lógica OR de tres variables.
+	"""
+
+	def __init__(self):
+		"""
+		Inicializa el perceptrón usando como función de activación la función
+		escalón.
+		"""
+		super (PerceptronOR, self).__init__ (3,lambda x: 0 if x < 0 else 1)
+		self.tabla_or = map(list, product (*([[0,1]]*3)))
+		self.salida = [0] + [1 for _ in range(7)]
